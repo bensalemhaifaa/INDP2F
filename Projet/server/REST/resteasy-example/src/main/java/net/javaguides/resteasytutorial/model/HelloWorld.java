b03f158947a28f0b23ae6edbf9a4c7157849e45f
bensalemhaifaa <83189631+bensalemhaifaa@users.noreply.github.com>
2021-05-16 22:10:55 +0100

    Add files via upload

---
+++ Projet/server/REST/resteasy-example/src/main/java/net/javaguides/resteasytutorial/model/HelloWorld.java
@@ -0,0 +1,15 @@
+package net.javaguides.resteasytutorial.model;
+import javax.xml. bind.annotation.XmlRootElement;
+@XmlRootElement 
+public class HelloWorld {
+ private String message;
+public HelloWorld(String message) 
+{super();
+this.message= message;
+}
+public String getMessage() { 
+return message;
+}
+public void setMessage(String message) { this.message = message;
+}
+}
